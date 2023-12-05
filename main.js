902b7dfa86997b60252862c4ce203b4416933ae8
Roberto Ramirez <robert.624@hotmail.com>
2023-12-05 14:37:26 -0500

    Add QR code functionality to form

---
+++ main.js
@@ -0,0 +1,18 @@
+const formUrl = document.querySelector("form.url");
+
+
+
+formUrl.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const url = formUrl.querySelector("input").value;
+
+    if(!url) {
+      alert("Please enter a URL");
+        return;
+    }
+
+    sessionStorage.setItem("url", url);
+    return window.location.assign("/qrcode.html");
+
+});
